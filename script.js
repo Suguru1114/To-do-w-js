1047c954df89f478f12e777af9e6904fc1d197cb
Suguru Miyata <suguru1114@gmail.com>
2024-09-05 22:30:10 +1000

    add style

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const inputTodo = document.getElementById("input-todo");
 const addTodo = document.getElementById("add-todo");
 const todocontainer = document.querySelector(".container");
 
+//adding tasks everytime button pressed
 addTodo.addEventListener("click", () => {
     displayTodo()
     inputTodo.value = "";
@@ -11,4 +12,14 @@ addTodo.addEventListener("click", () => {
 
 function displayTodo(){
     return;
+}
+
+function displayTodo(){
+    let todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    let todoText = document.createElement("p");
+    todoText.innerText = inputTodo.value;
+
+    console.log(todoText.innerText);
 }
