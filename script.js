d5dae754ebb6cf40f877836b4fe7bf3dc9511bbb
Suguru Miyata <suguru1114@gmail.com>
2024-09-10 08:21:10 +1000

    adding add function

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ addTodo.addEventListener("click", () => {
     // console.log(inputTodo.value);
 })
 
-function displayTodo(){
-    return;
-}
 
 function displayTodo(){
     let todo = document.createElement("div");
@@ -20,6 +17,19 @@ function displayTodo(){
 
     let todoText = document.createElement("p");
     todoText.innerText = inputTodo.value;
+
+    let todoActions = document.createElement("div");
+    todoActions.classList.add("actioins");
+
+    let checkbox  = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+
+    todoActions.appendChild(checkbox);
+    todoActions.appendChild(todoActions);
+
+    todocontainer.appendChild(todo);
 
     console.log(todoText.innerText);
 }
