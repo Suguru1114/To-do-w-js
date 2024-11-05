bd5037f5fb292281ccdd7b01f08b586a9e71e3ae
Suguru Miyata <suguru1114@gmail.com>
2024-11-06 00:00:11 +1100

    add write/addtodo

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,14 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+    const [todos, setTodos] = useState([]);
+    const [inputVal, setInputVal] = useState("");
+
+    function writeTodo(e){
+      setInputVal(e.target.value)
+      console.log(inputVal)
+
+    }
+
+  
+    function addTodo(){
+      setTodos(prevTodos => [])
+    }
 
   return (
     <main>
 
     <h1>Todo List React</h1>
     <div className="input-container">
-        <input id="input-todo" type="text" placeholder="" />
-        <button id ="add-todo">+</button>
+        <input 
+        type="text" 
+        placeholder="Enter Todo" 
+        onChange={writeTodo} 
+
+        />
+        <button onClick={addTodo}>+</button>
     
      </div> 
     <div className="container">
